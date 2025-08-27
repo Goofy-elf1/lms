e97347ad836e8a455762d1f8d7bb3728c381d451
Aditya Kaithwas <adityakaithwas69@gmail.com>
2025-08-27 20:40:29 +0530

    removed name beside logo for ui aligment

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,6 @@ const Navbar = () => {
             <Link href="/">
                 <div className="flex items-center gap-2.5 cursor-pointer">
                     <Image src="/images/logo.svg" alt="logo" width={46} height={44} />
-                    <span className="text-2xl font-bold">Kraken AI</span>
                 </div>
             </Link>
             <div className="flex items-center gap-8">
