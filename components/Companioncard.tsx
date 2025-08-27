0633d24d618a7ea0d080ea3eb70e6baefcfac889
Aditya Kaithwas <adityakaithwas69@gmail.com>
2025-08-27 20:24:53 +0530

    updated logo

--- components/Companioncard.tsx
+++ components/Companioncard.tsx
@@ -16,7 +16,7 @@ const companioncard = ({id, name, topic, subject, duration, color}:
     <article className="companion-card" style={{backgroundColor: color}}>
      <div className=" flex justify-between items-center">
       <div className="subject-badge">{subject}</div>
-      <button className="companioncard-bookmark">
+      <button className="companion-bookmark">
         <Image src="/icons/bookmark.svg" alt="bookmark" width={12.5} height={15}/>
       </button>
      </div>
