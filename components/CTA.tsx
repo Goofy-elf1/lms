838832083d4ef931dfd94be1054033b07607e510
Aditya Kaithwas <adityakaithwas69@gmail.com>
2025-09-15 14:07:54 +0530

    added cta text

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,7 +1,16 @@
 const CTA = () => {
   return (
     <section className="cta-section">
-      CTA
+      <div className="cta-badge">
+        Start Learning Your Way.</div>
+        <h2 className="text-3xl font-bold">
+          Build and Personalize Learning Companion
+        </h2>
+        <p>
+          "Pick a name, subject, 
+          voice, & personality — and start 
+          learning through voice conversations that feel natural and fun."
+        </p>
     </section>
   )
 }
