23f95e855221905158fc34158d7680c051bcb835
Aditya Kaithwas <adityakaithwas69@gmail.com>
2025-09-22 10:20:38 +0530

    stated integrating voice agent ui and animation

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -1,8 +1,13 @@
 'use client' 
 
 import { getSubjectColor } from '@/lib/utils'
-import React, { useEffect, useState } from 'react'
-
+import { vapi } from '@/lib/vapi.sdk';
+import React, { useEffect, useRef, useState } from 'react'
+import { cn } from "@/lib/utils";
+import { ca } from 'zod/locales';
+import Image from 'next/image';
+import Lottie, { LottieRef, LottieRefCurrentProps } from 'lottie-react';
+import soundwaves from '@/constants/soundwaves.json';
 enum CallStatus {
     INACTIVE = 'INACTIVE',
     CONNECTING = 'CONNECTING',
@@ -14,15 +19,45 @@ const CompanionComponent =
 ({ companionId, subject, topic, name, userName, userImage, style, voice}: CompanionComponentProps) => {
 
     const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
+    const [ isSpeaking, setIsSpeaking] = useState(false);
+
+    const lottieRef = useRef<LottieRefCurrentProps>(null);
+
+    useEffect(() => {
+        if(lottieRef) {
+          if(isSpeaking) lottieRef.current?.play()
+            else lottieRef.current?.stop()
+        }
+    }, [isSpeaking, lottieRef])
 
     useEffect(() => {
         const onCallStart = () => setCallStatus(CallStatus.ACTIVE);
 
         const onCallEnd = () => setCallStatus(CallStatus.FINISHED);
 
-        const onMessage = {}
+        const onMessage = () => {}
 
-        const onError = (error: Error) => console.log('Error', error); 
+        const onSpeechStart = () => setIsSpeaking(true);
+        const onSpeechEnd = () => setIsSpeaking(false);
+
+        const onError = (error: Error) => console.log('Error', error);
+        
+        vapi.on('call-start', onCallStart);
+        vapi.on('call-end', onCallEnd);
+        vapi.on('message', onMessage);
+        vapi.on('error', onError);
+        vapi.on('speech-start', onSpeechStart);
+        vapi.on('speech-end', onSpeechEnd);
+
+        return () => {
+        vapi.off('call-start', onCallStart);
+        vapi.off('call-end', onCallEnd);
+        vapi.off('message', onMessage);
+        vapi.off('error', onError);
+        vapi.off('speech-start', onSpeechStart);
+        vapi.off('speech-end', onSpeechEnd);
+        }
+
     }, []);
 
   return (
@@ -30,10 +65,26 @@ const CompanionComponent =
        <section className='flex gap-8 max-sm:flex-col'>
             <div className='companion-section'>
                 <div className='companion-avatar' style={{backgroundColor: getSubjectColor(subject)}}>
-                     <div className='{cn(absolute transition-opacity duration-1000)}'>
+                     <div className={cn('absolute transition-opacity duration-1000',
+                      callStatus === CallStatus.FINISHED || 
+                      callStatus === CallStatus.INACTIVE ? 'opacity-100' : 'opacity-0',
+                      callStatus === CallStatus.ACTIVE && 'opacity-100 animate-pulse'
+                     )}>
+                      <Image src={`/icons/${subject}.svg`} alt={subject} width={150} height={150}
+                      className='max-sm:w-fit' />
+                     </div>
 
+                     <div className={cn('absolute transition-opacity duration-1000', 
+                     callStatus === CallStatus.ACTIVE ? 'opacity-100' : 'opacity-0')}>
+                        <Lottie lottieRef={lottieRef}
+                        animationData={soundwaves}
+                        autoPlay={false}
+                        className='companion-lottie'
+                        />
                      </div>
                 </div>
+                      
+
             </div>
        </section>
     </section>
